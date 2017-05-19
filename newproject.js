c9cc88a576ebe9aaf59f6d423dcf2a0e865c7b22
ani98 <agrigoryan.98@mail.ru>
2017-05-19 21:10:39 +0400

    create server

--- newproject.js
+++ newproject.js
@@ -10,8 +10,18 @@ sock.on('connect', () => {
 sock.setEncoding('utf-8');
 const total_data = [];
 sock.on('data', d =>total_data.push(d));
-sock.on('end',() => console.log(total_data.reduce((prev, total) => prev + total )));
+sock.on('end',() => {const result = total_data.reduce((prev, total) => prev + total );
+console.log(f(result))});
 sock.connect({
     port: 80,
     host: 'istc.am'
   });
+  const f = (d) => {
+    const arr = d.split('\r\n\r\n');
+    const headers = arr[0].split('\r\n');
+    const body = arr[1];
+    return {
+      headers,
+      body
+    }
+  }
