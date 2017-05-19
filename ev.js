e281563d7c33e37ff2b06f5bc09f269043119c2f
ani98 <agrigoryan.98@mail.ru>
2017-05-19 19:12:00 +0400

    create server

--- ev.js
+++ ev.js
@@ -1,17 +1,20 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((rq,res)=>{
-  if (rq.url === '/'){
+const {port, upper} = require('./project');
+const [a,b] = [1,2];
+const server = http.createServer((req,res)=>{
+  if (req.url === '/'){
     res.setHeader('content-type','text/html');
     res.end(`
       <!doctype html>
       <body>
-      <h1>some headerl <h1>
-      <p> whot a body =p </p>
-      <body>
+        <h1> ${upper(__dirname)} </h1>
+        <h1> ${port} </h1>
+        <p> what a body =P </p>
+      </body>
       `)
   } else {
-    res.end('hoho')
+    res.end('Unknown')
   }
 })
-server.listen(8080,()=> console.log('started our server'));
+server.listen(port,()=> console.log('started our server'));
